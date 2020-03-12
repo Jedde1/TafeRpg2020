77c9c1e8a312cf34f32b9a0963de0db9a60e5968
Jedde1 <54158726+Jedde1@users.noreply.github.com>
2020-03-12 16:04:58 +1100

    Keybind and Options Menu

---
+++ Assets/Scripts/Menu/KeyBindManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBindManager : MonoBehaviour
+{
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+    public Text forward, left, right, backward, jump, sprint, crouch;
+    public GameObject curKey;
+    public Color32 changed = new Color32(39, 171, 249, 255);
+    public Color32 selected = new Color32(239, 116, 36, 255);
+    void Start()
+    {
+        keys.Add("Forward", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Forward", "W")));
+        keys.Add("Left", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left", "A")));
+        keys.Add("Right", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right", "D")));
+        keys.Add("Backward", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Backward", "S")));
+        keys.Add("Jump", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Jump", "Space")));
+        keys.Add("Sprint", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Sprint", "LeftShift")));
+        keys.Add("Crouch", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Crouch", "LeftControl")));
+
+        forward.text = keys["Forward"].ToString();
+        left.text = keys["Left"].ToString();
+        right.text = keys["Right"].ToString();
+        backward.text = keys["Backward"].ToString();
+        jump.text = keys["Jump"].ToString();
+        sprint.text = keys["Sprint"].ToString();
+        crouch.text = keys["Crouch"].ToString();
+    }
+
+    void OnGUI()
+    {
+        if (curKey != null)
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                keys[curKey.name] = e.keyCode;
+                curKey.GetComponentInChildren<Text>().text = e.keyCode.ToString();
+                curKey.GetComponent<Image>().color = changed;
+                curKey = null;
+            }
+        }
+    }
+    public void ChangeKey(GameObject clicked)
+    {
+        if (curKey != null)
+        {
+            curKey.GetComponent<Image>().color = changed;
+        }
+        curKey = clicked;
+        curKey.GetComponent<Image>().color = selected;
+    }
+    public void SaveKeys()
+    {
+        foreach (var key in keys)
+        {
+            PlayerPrefs.SetString(key.Key, key.Value.ToString());
+        }
+        PlayerPrefs.Save();
+    }
+}
--- Assets/Scripts/Menu/OptionsMenu.cs
+++ Assets/Scripts/Menu/OptionsMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
@@ -13,5 +14,50 @@ public class OptionsMenu : MonoBehaviour
         audioMixer.SetFloat("Volume", volume);
     }
     #endregion
+    #region Set Quality
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    #endregion
+    #region Set Fullscreen
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    #endregion
+    #region Set Resolution
+    public Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
 
+    private void Start()
+    {
+        //Get Monitors resolution
+        resolutions = Screen.resolutions;
+        //empty drop down
+        resolutionDropdown.ClearOptions();
+        //Create a list of strings
+        List<string> options = new List<string>();
+        //indes of our current index
+        int currentResolutionIndex = 0;
+        //loop through and create option for the list
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = resolutions[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+    #endregion
 }
